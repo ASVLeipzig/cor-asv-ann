12068fde09dab75d3cbe0e19656ffbe478cdbfb4
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-02-05 02:14:51 +0100

    evaluate: add param match_on, allow comparing across @index of same fileGrp, too

--- ocrd_cor_asv_ann/wrapper/evaluate.py
+++ ocrd_cor_asv_ann/wrapper/evaluate.py
@@ -33,17 +33,29 @@ class EvaluateLines(Processor):
         super().__init__(*args, **kwargs)
         
     def process(self):
-        """Align textlines of multiple file groups and calculate distances.
-        
-        Find files in all input file groups of the workspace for the same
-        pageIds. The first file group serves as reference annotation (ground truth).
+        """Align textlines of alternative annotations and calculate distances for error rates.
+
+        \b
+        Alternative input annotations derive from either:
+        - multiple ``TextEquiv/@index`` within one file group and file, or
+        - multiple file groups (i.e. multiple files for the same page ID, with
+          the same TextLine id or coords, using only the first TextEquiv).
+
+        The first annotation (i.e. index or file group) serves as reference
+        (ground truth).
         
         Open and deserialise PAGE input files, then iterate over the element
-        hierarchy down to the TextLine level, looking at each first TextEquiv.
-        Align character sequences in all pairs of lines for the same TextLine IDs,
+        hierarchy down to the TextLine level.
+
+        \b
+        Now, align character sequences in all pairs of lines for the same 
+        - TextLine IDs across files (if `match_on=id`) or 
+        - TextLine boundary points across files (if `match_on=coords`) or 
+        - TextLine baseline points across files (if `match_on=baseline`) or
+        - ``TextLine/TextEquiv/@index`` (if `match_on=index`),
         and calculate the distances using the error metric `metric`. Accumulate
         distances and sequence lengths per file group globally and per file,
-        and show each fraction as a CER rate in the log.
+        and show each fraction as a CER and WER rate in the output JSON reports.
         """
         assert_file_grp_cardinality(self.output_file_grp, 1)
 
@@ -56,16 +68,20 @@ class EvaluateLines(Processor):
         LOG.info('Using evaluation metric "%s".', metric)
         if metric == 'Levenshtein-fast' and confusion > 0:
             LOG.warning('There will be no confusion statistics with this metric.')
+        match = self.parameter['match_on']
         
         ifgs = self.input_file_grp.split(",") # input file groups
-        if len(ifgs) < 2:
-            raise Exception("need multiple input file groups to compare")
+        if match == 'index':
+            assert_file_grp_cardinality(self.input_file_grp, 1,
+                                        msg="only 1 input fileGrp when match_on=index")
+        elif len(ifgs) < 2:
+            raise Exception("need multiple input file groups to compare when match_on!=index")
         ifts = self.zip_input_files(mimetype=MIMETYPE_PAGE) # input file tuples
 
         # get separate aligners (1 more than needed), because
         # they are stateful (confusion counts):
-        self.caligners = [Alignment(logger=LOG, confusion=bool(confusion)) for _ in ifgs]
-        self.waligners = [Alignment(logger=LOG) for _ in ifgs]
+        caligners = [Alignment(logger=LOG, confusion=bool(confusion)) for _ in ifgs]
+        waligners = [Alignment(logger=LOG) for _ in ifgs]
         
         # running edit counts/mean/variance for each file group:
         cedits = [Edits(logger=LOG, histogram=histogram) for _ in ifgs]
@@ -76,36 +92,66 @@ class EvaluateLines(Processor):
             file_cedits = [Edits(logger=LOG, histogram=histogram) for _ in ifgs]
             file_wedits = [Edits(logger=LOG) for _ in ifgs]
             # get input lines:
-            file_lines = [{} for _ in ifgs] # line dicts for this file
-            for i, input_file in enumerate(ift):
-                if not i:
-                    LOG.info("processing page %s", input_file.pageId)
-                if not input_file:
-                    # file/page was not found in this group
-                    continue
-                LOG.info("INPUT FILE for %s: %s", ifgs[i], input_file.ID)
+            if match == 'index':
+                input_file = ift[0]
+                LOG.info("processing page %s", input_file.pageId)
+                LOG.info("INPUT FILE: %s", input_file.ID)
                 pcgts = page_from_file(self.workspace.download_file(input_file))
-                file_lines[i] = page_get_lines(pcgts)
-            # compare lines with GT:
-            report = dict()
-            for line_id in file_lines[0].keys():
+                line_indexes = page_get_lines(pcgts, match)
+                line_ids = line_indexes.keys()
+                file_lines = []
+                for line_id in line_ids:
+                    indexes = line_indexes[line_id].keys()
+                    if not indexes:
+                        continue
+                    for _ in range(len(file_lines), max(indexes) + 1):
+                        file_lines.append(dict())
+                    for _ in range(len(cedits), max(indexes) + 1):
+                        cedits.append(Edits(logger=LOG, histogram=histogram))
+                        wedits.append(Edits(logger=LOG))
+                        caligners.append(Alignment(logger=LOG, confusion=bool(confusion)))
+                        waligners.append(Alignment(logger=LOG))
+                    for _ in range(len(file_cedits), max(indexes) + 1):
+                        file_cedits.append(Edits(logger=LOG, histogram=histogram))
+                        file_wedits.append(Edits(logger=LOG))
+                    for index in indexes:
+                        file_lines[index][line_id] = line_indexes[line_id][index]
+            else:
+                file_lines = [{} for _ in ifgs] # line dicts for this file
                 for i, input_file in enumerate(ift):
                     if not i:
-                        continue
-                    pair = ifgs[0] + ',' + ifgs[i]
-                    lines = report.setdefault(pair, dict()).setdefault('lines', list())
+                        LOG.info("processing page %s", input_file.pageId)
                     if not input_file:
                         # file/page was not found in this group
                         continue
-                    elif line_id not in file_lines[i]:
+                    LOG.info("INPUT FILE for %s: %s", ifgs[i], input_file.ID)
+                    pcgts = page_from_file(self.workspace.download_file(input_file))
+                    file_lines[i] = page_get_lines(pcgts, match)
+            # compare lines with GT:
+            report = dict()
+            gt_lines = file_lines[0]
+            for line_id in gt_lines.keys():
+                for i, input_lines in enumerate(file_lines):
+                    if not i:
+                        continue
+                    if match == 'index':
+                        pair = '0,%d' % i
+                    else:
+                        pair = ifgs[0] + ',' + ifgs[i]
+                        input_file = ift[i]
+                    lines = report.setdefault(pair, dict()).setdefault('lines', list())
+                    if not input_lines or not input_file:
+                        # file/page was not found in this group
+                        continue
+                    elif line_id not in input_lines:
                         LOG.error('line "%s" in file "%s" is missing from input %d',
                                   line_id, input_file.ID, i)
                         lines.append({line_id: 'missing'})
                         continue
-                    gt_line = file_lines[0][line_id]
+                    gt_line = gt_lines[line_id]
                     gt_len = len(gt_line)
                     gt_words = splitwords(gt_line)
-                    ocr_line = file_lines[i][line_id]
+                    ocr_line = input_lines[line_id]
                     ocr_len = len(ocr_line)
                     ocr_words = splitwords(ocr_line)
                     if 0.2 * (gt_len + ocr_len) < math.fabs(gt_len - ocr_len) > 5:
@@ -113,16 +159,16 @@ class EvaluateLines(Processor):
                                     line_id, input_file.ID, gt_len, ocr_len)
                     if metric == 'Levenshtein-fast':
                         # not exact (but fast): codepoints
-                        cdist, clen = self.caligners[i].get_levenshtein_distance(ocr_line, gt_line)
-                        wdist, wlen = self.waligners[i].get_levenshtein_distance(ocr_words, gt_words)
+                        cdist, clen = caligners[i].get_levenshtein_distance(ocr_line, gt_line)
+                        wdist, wlen = waligners[i].get_levenshtein_distance(ocr_words, gt_words)
                     else:
                         # exact (but slow): grapheme clusters
-                        cdist, clen = self.caligners[i].get_adjusted_distance(
+                        cdist, clen = caligners[i].get_adjusted_distance(
                             ocr_line, gt_line,
                             # Levenshtein / NFC / NFKC / historic_latin
                             normalization=metric,
                             gtlevel=gtlevel)
-                        wdist, wlen = self.waligners[i].get_adjusted_distance(
+                        wdist, wlen = waligners[i].get_adjusted_distance(
                             ocr_words, gt_words,
                             # Levenshtein / NFC / NFKC / historic_latin
                             normalization=metric,
@@ -139,18 +185,22 @@ class EvaluateLines(Processor):
                         'ocr': ocr_line}})
             
             # report results for file
-            for i, input_file in enumerate(ift):
+            for i, input_lines in enumerate(file_lines):
                 if not i:
                     continue
-                elif not input_file:
+                elif not input_lines:
                     # file/page was not found in this group
                     continue
-                LOG.info("%5d lines %.3f±%.3f CER %.3f±%.3f WER %s / %s vs %s",
-                         file_cedits[i].length,
+                if match == 'index':
+                    pair = '0,%d' % i
+                else:
+                    pair = ifgs[0] + ',' + ifgs[i]
+                    input_file = ift[i]
+                LOG.info("%5d lines %.3f±%.3f CER %.3f±%.3f WER %s / %s",
+                         file_cedits[i].steps,
                          file_cedits[i].mean, math.sqrt(file_cedits[i].varia),
                          file_wedits[i].mean, math.sqrt(file_wedits[i].varia),
-                         input_file.pageId, ifgs[0], ifgs[i])
-                pair = ifgs[0] + ',' + ifgs[i]
+                         input_file.pageId, pair)
                 report[pair] = {}
                 report[pair]['num-lines'] = file_cedits[i].steps
                 report[pair]['num-words'] = file_wedits[i].length
@@ -176,16 +226,24 @@ class EvaluateLines(Processor):
             
         # report overall results
         report = dict()
-        for i in range(1, len(ifgs)):
-            if not cedits[i].length:
-                LOG.warning('%s had no textlines whatsoever', ifgs[i])
+        for i in range(len(caligners)):
+            if not i:
                 continue
-            LOG.info("%5d lines %.3f±%.3f CER %.3f±%.3f WER overall / %s vs %s",
-                     cedits[i].length,
+            if match == 'index':
+                src = 'index %d' % i
+                pair = '0,%d' % i
+            else:
+                src = ifgs[i]
+                pair = ifgs[0] + ',' + ifgs[i]
+            if not cedits[i].steps:
+                LOG.warning('%s had no textlines whatsoever', src)
+                continue
+            LOG.info("%5d lines %.3f±%.3f CER %.3f±%.3f WER overall / %s",
+                     cedits[i].steps,
                      cedits[i].mean, math.sqrt(cedits[i].varia),
                      wedits[i].mean, math.sqrt(wedits[i].varia),
-                     ifgs[0], ifgs[i])
-            report[ifgs[0] + ',' + ifgs[i]] = {
+                     pair)
+            report[pair] = {
                 'num-lines': cedits[i].steps,
                 'num-words': wedits[i].length,
                 'num-chars': cedits[i].length,
@@ -194,22 +252,14 @@ class EvaluateLines(Processor):
                 'word-error-rate-mean': wedits[i].mean,
                 'word-error-rate-varia': wedits[i].varia,
             }
-        if confusion:
-            for i in range(1, len(ifgs)):
-                if not cedits[i].length:
-                    continue
-                conf = self.caligners[i].get_confusion(confusion)
-                LOG.info("most frequent confusion / %s vs %s: %s",
-                         ifgs[0], ifgs[i], conf)
-                report[ifgs[0] + ',' + ifgs[i]]['confusion'] = repr(conf)
-        if histogram:
-            for i in range(1, len(ifgs)):
-                if not cedits[i].length:
-                    continue
+            if confusion:
+                conf = caligners[i].get_confusion(confusion)
+                LOG.info("most frequent confusion / %s: %s", pair, conf)
+                report[pair]['confusion'] = repr(conf)
+            if histogram:
                 hist = cedits[i].hist()
-                LOG.info("character histograms / %s vs %s: %s",
-                         ifgs[0], ifgs[i], hist)
-                report[ifgs[0] + ',' + ifgs[i]]['histogram'] = repr(hist)
+                LOG.info("character histograms / %s: %s", pair, hist)
+                report[pair]['histogram'] = repr(hist)
         # write back result to overall report
         file_id = self.output_file_grp
         file_path = os.path.join(self.output_file_grp, file_id + '.json')
@@ -221,7 +271,18 @@ class EvaluateLines(Processor):
             mimetype='application/json',
             content=json.dumps(report, indent=2, ensure_ascii=False))
 
-def page_get_lines(pcgts):
+def _linekey(line, match_on):
+    if match_on == 'id':
+        return line.id
+    if match_on == 'baseline':
+        if line.Baseline is None:
+            LOG.error("cannot extract baseline from line '%s'", line.id)
+            return line.Coords.points
+        return line.Baseline.points
+    if match_on == 'coords':
+        return line.Coords.points
+
+def page_get_lines(pcgts, match_on):
     '''Get all TextLines in the page.
     
     Iterate the element hierarchy of the page `pcgts` down
@@ -245,6 +306,14 @@ def page_get_lines(pcgts):
             if not textequivs:
                 LOG.warning("Line '%s' contains no text results", line.id)
                 continue
-            result[line.id] = textequivs[0].Unicode
+            if match_on == 'index':
+                for i, textequiv in enumerate(textequivs):
+                    index = textequiv.index or i
+                    lined = result.setdefault(line.id, dict())
+                    if index in lined:
+                        LOG.warning("Line '%s' contains TextEquiv with and without @index", line.id)
+                    lined[index] = textequiv.Unicode
+            else:
+                result[_linekey(line, match_on)] = textequivs[0].Unicode
     return result
     
