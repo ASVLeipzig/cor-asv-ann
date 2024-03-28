ff6bf3f88f78906e7ffc99204839584f597bda09
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-03-28 01:11:11 +0100

    mark: try de-hyphenation; also log mismatch rate

--- ocrd_cor_asv_ann/wrapper/mark.py
+++ ocrd_cor_asv_ann/wrapper/mark.py
@@ -50,6 +50,11 @@ class MarkWords(Processor):
         command = self.parameter['command']
         format_ = self.parameter['format']
         n11n = self.parameter['normalization']
+        def run_command(text):
+            result = run(command, input=text, encoding="utf-8",
+                         text=True, shell=True, capture_output=True)
+            result.stdout = result.stdout.rstrip('\n')
+            return result
         def asword(token):
             # apply normalization
             for nfrom, nto in n11n.items():
@@ -61,16 +66,33 @@ class MarkWords(Processor):
                 if cat in 'LM':
                     result += char
             return result
+        def save(equiv):
+            # save old state of hyphenated word to undo marking it as non-match
+            if format_ == 'conf':
+                oldattr = 'conf'
+            else:
+                oldattr = 'comments'
+            oldvalue = getattr(equiv, oldattr)
+            def fun():
+                setattr(equiv, oldattr, oldvalue)
+                LOG.debug("Hyphenated word '%s' will be unmarked: '%s'", equiv.parent_object_.id, equiv.Unicode)
+            return fun
+        total_candidates = 0
+        total_nonmatches = 0
         for n, input_file in enumerate(self.input_files):
+            file_candidates = 0
+            file_nonmatches = 0
             LOG.info("INPUT FILE %i / %s", n, input_file.pageId or input_file.ID)
             pcgts = page_from_file(self.workspace.download_file(input_file))
             page_id = input_file.pageId or input_file.ID
             file_id = make_file_id(input_file, self.output_file_grp)
             pcgts.set_pcGtsId(file_id)
             self.add_metadata(pcgts)
+            undo = None
             for region in pcgts.get_Page().get_AllRegions(classes=['Text']):
                 for line in region.get_TextLine():
-                    for word in line.get_Word():
+                    words = line.get_Word()
+                    for word in words:
                         equiv = word.get_TextEquiv()
                         if not len(equiv):
                             LOG.warning("Word '%s' contains no text results", word.id)
@@ -83,25 +105,50 @@ class MarkWords(Processor):
                         if not text0:
                             LOG.debug("Word '%s' has no letters: '%s'", word.id, text)
                             continue
-                        text = text0
-                        result = run(command, input=text, encoding="utf-8", universal_newlines=True,
-                                     shell=True, stdout=PIPE, stderr=PIPE)
+                        result = run_command(text0)
+                        file_candidates += 1
                         if result.returncode != 0:
-                            LOG.error("Word '%s' lookup failed (%d): '%s'", word.id, result.returncode, result.stderr)
-                            continue
-                        result.stdout = result.stdout.rstrip('\n')
-                        if result.stdout:
-                            assert text == result.stdout, (text, result.stdout)
-                            LOG.debug("Word '%s' will be marked: '%s'", word.id, text)
+                            LOG.error("Word '%s' lookup failed (%d): '%s'", text0, result.returncode, result.stderr)
+                        elif result.stdout:
+                            assert text0 == result.stdout, (text0, result.stdout)
+                            if undo and word is words[0]:
+                                # try with dehyphenation
+                                undo, text0 = undo
+                                text = text0 + text
+                                text0 = asword(text)
+                                result = run_command(text0)
+                                if result.returncode != 0:
+                                    LOG.error("Word '%s' lookup failed (%d): '%s'", text0, result.returncode, result.stderr)
+                                elif not result.stdout:
+                                    # undo previous word, skip this word
+                                    undo()
+                                    undo = None
+                                    file_nonmatches -= 1
+                                    continue
+                            undo = None
+                            if word is words[-1] and (text.endswith('-') or text.endswith('⸗')):
+                                # save hyphenation candidate
+                                undo = save(equiv[0]), text[:-1]
+                            LOG.debug("Word '%s' will be marked: '%s'", word.id, text0)
+                            file_nonmatches += 1
                             if format_ == 'conf':
                                 equiv[0].conf = 0.123
                             else:
                                 equiv[0].comments = format_
+            LOG.info("marked %d unmatched words out of %d tokens (%d%%) on %s",
+                     file_nonmatches, file_candidates,
+                     100 * file_nonmatches / file_candidates if file_candidates else 0,
+                     input_file.pageId)
+            total_candidates += file_candidates
+            total_nonmatches += file_nonmatches
             file_path = os.path.join(self.output_file_grp, file_id + '.xml')
             self.workspace.add_file(
-                ID=file_id,
+                file_id=file_id,
                 file_grp=self.output_file_grp,
-                pageId=input_file.pageId,
+                page_id=input_file.pageId,
                 local_filename=file_path,
                 mimetype=MIMETYPE_PAGE,
                 content=to_xml(pcgts))
+        LOG.info("marked %d unmatched words out of %d tokens (%d%%) overall",
+                 total_nonmatches, total_candidates,
+                 100 * total_nonmatches / total_candidates if total_candidates else 0)
